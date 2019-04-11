2839d070944e4cd60c948d0109daa38910ebf683
sdialogo <sdialogo@micronsystems.com>
2019-04-11 13:23:15 +0800

    working star-match game

---
+++ src/components/StarDisplay.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+import utils from '../math-utils';
+const StarsDisplay = props => (
+  <>
+    {utils.range(1, props.count).map(starId => (
+      <div key={starId} className="star" />
+    ))}
+  </>
+);
+
+export default StarsDisplay;
